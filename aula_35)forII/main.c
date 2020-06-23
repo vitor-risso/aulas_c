6f476b08d0464b67f23ed3b6bc5cebaf0d0ef5ab
vitor-risso <vitor.risso13@gmail.com>
2020-06-23 14:07:36 -0300

    aula35

---
+++ aula_35)forII/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main (){
+
+
+    for (int a = 100, b = 0; a >=0 && b <= 100 ; --a , ++b  )
+    {
+        printf ("%d", a);
+        printf(" - ");
+        printf("%d\n", b);
+
+
+
+}
+
+return 0;
+}
